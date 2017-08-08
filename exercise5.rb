b0e1f11ce0aa5ec27d5f4e160d624f0989f146a0
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-08 17:39:35 -0400

    added new file and finished excesise 5 and 6.1

--- exercise5.rb
+++ exercise5.rb
@@ -7,7 +7,6 @@ choice3 = "go home"
 while distance >= 0
 if user_choice == choice1
   puts "distance from home is #{distance += 1}"
-  puts "Would you like to walk or run?"
   user_choice = gets.chomp.downcase
 elsif user_choice == choice2
   puts "distance from home is #{distance += 5}"
