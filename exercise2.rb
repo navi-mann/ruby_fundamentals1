3620c57d4684e8a90487ec92a1d29691b3e55a0c
Navi Mann <navdeep.k.mann@gmail.com>
2017-08-08 14:30:55 -0400

    compelted excercise 2

---
+++ exercise2.rb
@@ -0,0 +1,9 @@
+#How would you calculate a good tip for a 55 dollar meal? Use puts to print the answer.
+puts "You should tip $#{55 * 0.18}."
+
+#Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result
+puts "My new purse cost me $" + 500.to_s
+#Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+puts "The result of 45628 * 7839 is #{45628 * 7839}"
+#What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+puts (10 < 20 && 30 < 20) || !(10 == 11)
